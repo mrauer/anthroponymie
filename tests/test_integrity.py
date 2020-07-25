4d8a449816d1091fc3effaddcbdbcaa55d32687e
Maxime Rauer <maxime.rauer@gmail.com>
2020-07-25 14:08:19 -0500

    [MID] Collect all countries (#7)
    * [MID] Collect all countries
    
    * With tests
    
    * All names with tests

--- tests/test_integrity.py
+++ tests/test_integrity.py
@@ -1,6 +1,8 @@
+import operator
+
 import pytest
 
-from lib import api, subset
+from lib import api, extract, subset
 
 
 class TestIntegrity():
@@ -8,8 +10,10 @@ class TestIntegrity():
     def setup(self):
         s = subset.Subset()
         w = api.WikiAPI()
+        e = extract.Extract()
         self.names = s.open_names('0.85')
         self.urls = w.open_urls()
+        self.frequencies = e.open_frequencies()
 
     def test_names_count(self):
         assert len(self.names) == 780
@@ -52,3 +56,44 @@ class TestIntegrity():
 
     def test_count_total_names(self):
         assert sum(self.names.values()) == 72125408
+
+    # @pytest.mark.skip(reason="must return 0")
+    def test_has_no_freq(self):
+        d = self.frequencies
+        no_data = dict()
+        for k, v in d.items():
+            if len(v) == 0:
+                no_data[k] = v
+        assert no_data == 0
+        # https://en.wikipedia.org/w/api.php?action=opensearch&search=TIMEO&limit=100&namespace=0&format=jsonfm
+
+    def test_name_is_foreign(self):
+        d = self.frequencies
+        data = dict()
+        for k, v in d.items():
+            try:
+                country = max(v.items(), key=operator.itemgetter(1))[0]
+                if country != 'FR':
+                    data[k] = country
+            except Exception:
+                continue
+        assert len(data) > 1
+
+    def test_distinct_stored_countries(self):
+        d = self.frequencies
+        countries = set()
+        for _, v in d.items():
+            for k, w in v.items():
+                countries.add(k)
+        assert len(countries) == 169
+
+    def test_distinct_stored_countries_popularity(self):
+        d = self.frequencies
+        countries = dict()
+        for _, v in d.items():
+            for k, w in v.items():
+                if k not in countries:
+                    countries[k] = 1
+                else:
+                    countries[k] += 1
+        assert len(countries) == 169
