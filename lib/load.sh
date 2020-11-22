fc375555d95c4ef498563b434c778755a4402f0a
Maxime Rauer <maxime.rauer@gmail.com>
2020-11-22 17:56:11 -0600

    for review

--- lib/load.sh
+++ lib/load.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-source="data/nat2018_csv.zip"
-if ! [ -f data/nat2018.csv ]; then
-  echo "Downloading source file"
-  curl --output $source https://www.insee.fr/fr/statistiques/fichier/2540004/nat2018_csv.zip
-  unzip $source -d data/ && rm $source
-  echo "Source file downloaded"
-fi
 
+# Source: https://www.insee.fr/fr/statistiques/fichier/2540004/nat2018_csv.zip
 # Copy data from S3
-aws s3 cp s3://$BUCKET_NAME/NAMES_$PERCENT.p data/
-aws s3 cp s3://$BUCKET_NAME/URLS.p data/
+aws s3 cp s3://"$BUCKET_NAME"/NAMES_"$PERCENT".p data/
+aws s3 cp s3://"$BUCKET_NAME"/URLS.p data/
+aws s3 cp s3://"$BUCKET_NAME"/nat2018.csv data/
--- lib/save.sh
+++ lib/save.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-aws s3 cp data/URLS.p s3://$BUCKET_NAME
-aws s3 cp data/NAMES_$PERCENT.p  s3://$BUCKET_NAME
+aws s3 cp data/URLS.p s3://"$BUCKET_NAME"
+aws s3 cp data/NAMES_"$PERCENT".p  s3://"$BUCKET_NAME"
+aws s3 cp data/nat2018.csv s3://"$BUCKET_NAME"
