224d4043f244fecc681454597c20e3ea5adcb381
Maxime Rauer <maxime.rauer@gmail.com>
2020-11-26 11:46:20 -0600

    [MID] Countries Logic Change (#19)

--- lib/load.sh
+++ lib/load.sh
@@ -3,5 +3,6 @@
 # Source: https://www.insee.fr/fr/statistiques/fichier/2540004/nat2018_csv.zip
 # Copy data from S3
 aws s3 cp s3://"$BUCKET_NAME"/NAMES_"$PERCENT".p data/
+aws s3 cp s3://"$BUCKET_NAME"/FREQUENCY.p data/
 aws s3 cp s3://"$BUCKET_NAME"/URLS.p data/
 aws s3 cp s3://"$BUCKET_NAME"/nat2018.csv data/
--- lib/save.sh
+++ lib/save.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 aws s3 cp data/URLS.p s3://"$BUCKET_NAME"
+aws s3 cp data/FREQUENCY.p s3://"$BUCKET_NAME"
 aws s3 cp data/NAMES_"$PERCENT".p  s3://"$BUCKET_NAME"
 aws s3 cp data/nat2018.csv s3://"$BUCKET_NAME"
